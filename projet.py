4f3d330f539a4ae48e1fd2ccd6977367cc0c8d75
TAHAHS <taha.hssine21@gmail.com>
2025-07-09 18:37:30 +0200

    Merging is successfully executed

--- projet.py
+++ projet.py
@@ -6,6 +6,15 @@ def produit(T):
 	return s
 =======
 data=[1,3,5]
+<<<<<<< HEAD
 prod=math.prod(data)
 print('le produit est   : ',prod)
+>>>>>>> projete1
+=======
+if data :
+	print ('le produit est :', prod(data))
+	print ('le min est :', min(data))
+	print ('le max est :', max(data))
+else :
+	print ('dossier vide')
 >>>>>>> projete1
