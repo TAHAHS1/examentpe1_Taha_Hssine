987b02c07f31e8d65eee9a9ef9e64d7e38ef73dc
TAHAHS <taha.hssine21@gmail.com>
2025-07-09 18:34:44 +0200

    Ajout du fichier projet.py after 14 is done

--- projet.py
+++ projet.py
@@ -1,4 +1,7 @@
 data=[1,3,5]
-print ('le produit est :', prod(data))
-print ('le min est :', min(data))
-print ('le max est :', max(data))
+if data :
+	print ('le produit est :', prod(data))
+	print ('le min est :', min(data))
+	print ('le max est :', max(data))
+else :
+	print ('dossier vide')
